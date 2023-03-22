88645d78b9701614732cc6ca4665713d895c2a86
choiseon <com1368@naver.com>
2023-03-23 00:18:36 +0900

    feat : http 구현하여 계산기 프로그램 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
     }
 }
