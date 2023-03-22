7ce3aa8fed56154c3fd57ac1c25251d8434cf48a
choiseon <com1368@naver.com>
2023-03-23 00:17:13 +0900

    feat : 사칙연산 구현

---
+++ src/main/java/org/example/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package org.example.calculator;
+
+import java.util.List;
+
+public class Calculator {
+
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
+
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return ArithmeticOperator.calculate(operand1.toInt(), operator, operand2.toInt());
+    }
+
+    public static int calculate2(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+                .mapToInt(arithmeticOperator -> arithmeticOperator.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+    }
+}
